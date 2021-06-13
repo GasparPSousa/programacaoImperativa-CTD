e95cf2c82bedad4a0c0c4d230a3c88caaea43f08
GasparPSousa <gasparufrj@gmail.com>
2021-06-13 12:07:53 -0300

    Spread Operator - MicroDesafio02

---
+++ 02-javascriptAvancado/02-objetosLiterais/spreadOperatorMicroDesafio2.js
@@ -0,0 +1,14 @@
+// Retornando o maior número
+function maiorNumero(...params) {
+    console.log(Math.max(...params))
+}
+
+maiorNumero(8, 5001, 9, 10, 101, 1001)
+
+
+// Retornando o menor número
+function menorNumero(...params) {
+    console.log(Math.min(...params))
+}
+
+menorNumero(8, 5001, 9, 10, 101, 1001)
