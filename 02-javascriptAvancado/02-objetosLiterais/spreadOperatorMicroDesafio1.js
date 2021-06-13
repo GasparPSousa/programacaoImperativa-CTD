79e52a0c1a644045db8d21566fd9edcebccfe265
GasparPSousa <gasparufrj@gmail.com>
2021-06-13 11:57:22 -0300

    Spread Operator - MicroDesafio01

---
+++ 02-javascriptAvancado/02-objetosLiterais/spreadOperatorMicroDesafio1.js
@@ -0,0 +1,6 @@
+const numerosPrimos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+const numeros = [0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, ...numerosPrimos]
+
+
+console.log(numeros)
