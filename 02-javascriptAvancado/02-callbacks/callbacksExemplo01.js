90a72691fcccc2d5518ed75f9bab991ec7f40221
GasparPSousa <gasparufrj@gmail.com>
2021-06-13 08:19:54 -0300

    Deletando o arquivo callbacks1.js e comentando o arquivo callbacksExemplo01.js

--- 02-javascriptAvancado/02-callbacks/callbacksExemplo01.js
+++ 02-javascriptAvancado/02-callbacks/callbacksExemplo01.js
@@ -5,4 +5,13 @@ let subtrair = (n1, n2) => n1 -n2;
 
 let operacaoMatematica = (n1, n2, operacao) => operacao(n1, n2);
 
+// Passando a função somar() como callback
 console.log(operacaoMatematica(3, 4, somar))
+
+
+// Não precisa ser uma função criada, eu posso criar a função diretamente no local do parâmetro usando o conceito de função anônima
+console.log(operacaoMatematica(3, 4, (n1, n2) => n1 * n2 ))
+
+
+
+// O JavaScript trabalha com Callbacks e é uma forma muito utilizada no dia-a-dia.
