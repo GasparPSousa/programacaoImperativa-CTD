55a4896d1b423cb0780c40fe8f21b019f4a32e68
GasparPSousa <gasparufrj@gmail.com>
2021-06-14 17:12:54 -0300

    Fazendo o tratamento de erro da arrow function dividir da calculadora

--- 02-javascriptAvancado/01-funcoesAvancadas/02-callbacks/callbacksCalculadora.js
+++ 02-javascriptAvancado/01-funcoesAvancadas/02-callbacks/callbacksCalculadora.js
@@ -4,7 +4,14 @@ const subtrair = (numeroA, numeroB) => numeroA - numeroB;
 
 const multiplicar = (numeroA, numeroB) => numeroA * numeroB;
 
-const dividir = (numeroA, numeroB) => numeroA / numeroB;  // Depois pensar numa forma de fazer o tratamento quando numeroB = 0;
+const dividir = (numeroA, numeroB) => {
+    if (numeroB != 0) {
+        return numeroA/numeroB;
+    } else {
+        return "Impossível calcular";
+    }
+};
+                                        
 
 const calculadora = (numeroA, numeroB, operacao) => operacao(numeroA, numeroB);
 
@@ -12,6 +19,7 @@ console.log(calculadora(10, 5, somar))
 console.log(calculadora(10, 5, subtrair))
 console.log(calculadora(10, 5, multiplicar))
 console.log(calculadora(10, 5, dividir))
+console.log(calculadora(10, 0, dividir))
 
 
 
