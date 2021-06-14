42e7997ec77b67d3e32f46f4925edbdee7dd33fe
GasparPSousa <gasparufrj@gmail.com>
2021-06-14 18:02:58 -0300

    Arrow Function - Exercicio FizzBuzz

---
+++ 02-javascriptAvancado/aula07-funcoesAvancadas/fizzbuzz.js
@@ -0,0 +1,20 @@
+let fizzBuzz = (n1, n2) => {
+for(let i = 1; i <= 100; i++) {
+    if ((i % n1 == 0) && (i % n2 == 0)) {
+        console.log("FizzBuzz")
+    } else if (i % n1 == 0) {
+        console.log("Fizz")
+    } else if (i % n2 == 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+    }
+};
+
+
+// fizzBuzz(2, 5)
+// fizzBuzz(2, 7)
+fizzBuzz(3, 5)
+
+
