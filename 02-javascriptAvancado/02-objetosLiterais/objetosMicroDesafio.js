75be555fcf0f00b7535f69f03aa42b0510302a68
GasparPSousa <gasparufrj@gmail.com>
2021-06-15 20:03:15 -0300

    Melhorando a saida do programa

--- 02-javascriptAvancado/02-objetosLiterais/objetosMicroDesafio.js
+++ 02-javascriptAvancado/02-objetosLiterais/objetosMicroDesafio.js
@@ -2,15 +2,23 @@ function Restaurante(nome, cardapio) {
     this.nome = nome;
     this.cardapio = cardapio;
 
+
 };
 
-let cardapio = ['opção1', 'opção2', 'opção3']
+let cardapio = ["opção1", "opção2", "opção3", "opção4", "opção5"]
 let restaurante = new Restaurante('Bom Sabor', cardapio)
 
 const restauranteObjeto = {
 
     saudacao: function entrada(nome, cardapio) {
-        console.log(`Olá, seja bem vindo ao restaurante ${restaurante.nome}! Aqui está o cardápio ${restaurante.cardapio}.`)
+        if(restaurante.cardapio.length === 0) {
+            console.log(`Olá, seja bem vindo ao restaurante ${restaurante.nome}! \n Cardápio ainda não cadastrado!`)
+        } else {
+            console.log(`Olá, seja bem vindo ao restaurante ${restaurante.nome}! \nAqui está o cardápio:`)
+        }
+        for(prato of restaurante.cardapio) {
+            console.log(prato)
+        }
     }
 
 }
