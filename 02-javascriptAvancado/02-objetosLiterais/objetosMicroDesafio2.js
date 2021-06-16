c7bd3e2167106c339f22332ef928d6abe98eb019
GasparPSousa <gasparufrj@gmail.com>
2021-06-16 12:03:38 -0300

    Micro Desafio Meu cardapio sem usar funcao Construtora

---
+++ 02-javascriptAvancado/02-objetosLiterais/objetosMicroDesafio2.js
@@ -0,0 +1,18 @@
+const restaurante = {
+    nome: 'Bom Sabor',
+    cardapio: ["opção1", "opção2", "opção3", "opção4", "opção5"],
+
+    entrada: function saudacao(nome, cardapio) {
+        if(this.cardapio.length === 0) {
+            console.log(`Olá, seja bem vindo ao restaurante ${this.nome}! \nCardápio ainda não cadastrado!`)
+        } else {
+            console.log(`Olá, seja bem vindo ao restaurante ${this.nome}! \nAqui está o cardápio:`)
+        }
+        for(prato of this.cardapio) {
+            console.log(prato)
+        }
+    }
+}
+
+
+restaurante.entrada()
