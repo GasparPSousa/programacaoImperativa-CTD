7eef6261da05aa450139684ff8b40548d6c2fab5
GasparPSousa <gasparufrj@gmail.com>
2021-06-16 11:17:31 -0300

    Calculando o Fatorial de um numero usando o For

---
+++ 02-javascriptAvancado/aula07-funcoesAvancadas/fatorialFor.js
@@ -0,0 +1,18 @@
+let fatorial = n => {
+    fat = 1
+    for(let i = 1; i <= n; i++ ) {
+        fat *= i;
+    }
+    return fat
+}
+
+console.log(fatorial(1))
+console.log(fatorial(2))
+console.log(fatorial(3))
+console.log(fatorial(4))
+console.log(fatorial(5))
+console.log(fatorial(6))
+console.log(fatorial(7))
+console.log(fatorial(8))
+console.log(fatorial(9))
+console.log(fatorial(10))
