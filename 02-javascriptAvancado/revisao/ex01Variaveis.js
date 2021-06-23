d98d2ec158083851f8d2046ea4f9358047341120
GasparPSousa <gasparufrj@gmail.com>
2021-06-23 18:04:11 -0300

    pequena revisão do basicao

---
+++ 02-javascriptAvancado/revisao/ex01Variaveis.js
@@ -0,0 +1,33 @@
+/* Aula 01 - Revisão - 23/06/2021
+
+Variaveis
+
+var -> variavel usada em qualque lugar (acesso Global)
+let -> utilizados em partes do código (dentro do Scopo)
+const -> 
+
+*/ 
+
+let minhaPrimeiraVariavel = 'Alô Mundo!'
+console.log(minhaPrimeiraVariavel)
+
+
+let melhorTime = 'Mengão'
+let temMundial = 'Sim!'
+
+console.log('===================')
+console.log(`Nome do time...: ${melhorTime}`)
+
+let nome = 'Gaspar'
+let numeroA = 53;
+let numeroB = 94.7;
+let estouEstudando = true;
+
+
+//typeof
+console.log(typeof nome)
+console.log(typeof numeroA)
+console.log(typeof numeroB)
+console.log(typeof estouEstudando)
+
+
