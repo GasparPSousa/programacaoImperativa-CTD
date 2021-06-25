6eba0662fb8697afefa2906dbe9a92b84f5a8e9d
GasparPSousa <gasparufrj@gmail.com>
2021-06-25 20:41:35 -0300

    Revisão Armanzendo os Resultados

---
+++ 01-fundamentosDaProgramacao/fundamentosRevisao/03-funcoes/funcoes03.js
@@ -0,0 +1,11 @@
+// Armazenando os outputs das funções
+
+function multiplicar(number1, number2) {
+    return number1 * number2
+}
+
+
+// Armazendo os resultados
+let resultado = multiplicar(98,10)
+console.log(resultado)
+
