52517f5d2597e44ea1a35b2f9113608dec3dcc97
GasparPSousa <gasparufrj@gmail.com>
2021-06-25 22:31:24 -0300

    Revisando arrays

---
+++ 01-fundamentosDaProgramacao/fundamentosRevisao/05-arrays/arrays.js
@@ -0,0 +1,12 @@
+// Arrays
+// Aceita vários e todos os tipos de dados em uma única variável
+// Nos permite criar um conjuntos de dados ordenados.
+// Tem índices começando por 0
+// Para achar o tamanho do array usando lengTH
+
+
+let pessoa = ['Gaspar', 37, 1.78, true, ['Rock', 'corrida']]
+
+console.log(pessoa[0])
+console.log(pessoa[4])
+console.log(pessoa.length)
