da15c071fdeb46ed1a52a4a679ac1f88dcffdc35
GasparPSousa <gasparufrj@gmail.com>
2021-06-25 22:01:45 -0300

    Revisando Switch

---
+++ 01-fundamentosDaProgramacao/fundamentosRevisao/04-controleDeFluxo/switch.js
@@ -0,0 +1,24 @@
+// Switch
+
+let cincoOuDezAnos = function(idade) {
+    
+    // Criando o switch dentro da função
+    switch (idade) {
+        case 10:
+            console.log('Tem 10 anos.')
+            break;
+        case 5:
+            console.log('Tem 5 anos.')
+            break
+        default:
+            console.log('Tem idade diferente de 5 ou 10 anos.')
+    
+    }
+}
+
+cincoOuDezAnos(5)
+cincoOuDezAnos(7)
+cincoOuDezAnos(10)
+cincoOuDezAnos(15)
+
+
