74f85f70151663cb07390ff0d6c056f00e8682f7
GasparPSousa <gasparufrj@gmail.com>
2021-06-25 21:46:09 -0300

    Revisando Ternario

---
+++ 01-fundamentosDaProgramacao/fundamentosRevisao/04-controleDeFluxo/ternario.js
@@ -0,0 +1,13 @@
+// Ternário
+
+// Para o ternário é obrigatório colocar código na segunda expressão.
+// Se não quisermos que nada aconteça, podemos usar uma string vazia.
+
+
+
+
+
+
+let qualEOMaior = 4 > 10 ? 'O 4 é maior': 'O 10 é maior';
+
+console.log(qualEOMaior)
