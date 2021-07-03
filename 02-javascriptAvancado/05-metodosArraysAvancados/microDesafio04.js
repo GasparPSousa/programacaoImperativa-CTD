10e34fa6afb9590b2c9aa7773b1fb4b64c6d824a
GasparPSousa <gasparufrj@gmail.com>
2021-07-03 18:37:49 -0300

    Metodos de Arrays - MicroDesafio04

---
+++ 02-javascriptAvancado/05-metodosArraysAvancados/microDesafio04.js
@@ -0,0 +1,16 @@
+/** MicroDesafio04
+ * 
+ * Crie um array de animais, após isso passe por cada índice utilizando o .forEach() 
+ * e imprima a frase “O animal é NOME_DO_ANIMAL”.
+ * 
+ * 
+ * 
+ */
+
+let animals = ['Cachorro', 'Papagaio', 'Elefante', 'Gato', 'Tigre', 'Girafa']
+
+animals.forEach(animal => console.log(`O animal é ${animal}`))
+
+
+
+
