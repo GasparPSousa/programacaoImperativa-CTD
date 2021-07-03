de05996e71602b28db9d9ac8fed08fc4b942add3
GasparPSousa <gasparufrj@gmail.com>
2021-07-03 18:04:43 -0300

    Metodos de Arrays - MicroDesafio03

---
+++ 02-javascriptAvancado/05-metodosArraysAvancados/microDesafio03.js
@@ -0,0 +1,16 @@
+/** MicroDesafio03
+ * 
+ * Crie um array de números e utilize a função .reduce() para devolver uma string com os números formatados.
+
+    Exemplo [1,5,9,3,7] => “1 – 5 – 9 – 3 – 7”
+
+ * 
+ * 
+ * 
+ */
+
+let numbers = [1, 5, 9, 3, 7]
+
+let numbersFormatted = numbers.reduce((acc, number) => `${acc}-${number}`)
+
+console.log(numbersFormatted)
