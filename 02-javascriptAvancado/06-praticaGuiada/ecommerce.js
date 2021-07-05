a887b4c4e72aecb6585fab95f537a71fb9a5ec50
GasparPSousa <gasparufrj@gmail.com>
2021-07-04 23:55:12 -0300

    Ecommerce - Item1

---
+++ 02-javascriptAvancado/06-praticaGuiada/ecommerce.js
@@ -0,0 +1,30 @@
+// Item 1
+
+produtos = [
+    {
+    nomeDoProduto: 'Computador',
+    valorDoProduto: 5000,
+    qualidadeDoProduto: 90,
+    status: true
+    },
+
+    {
+    nomeDoProduto: 'Kindle',
+    valorDoProduto: 500,
+    qualidadeDoProduto: 95,
+    status: true
+    },
+
+    {
+    nomeDoProduto: 'Bateria',
+    valorDoProduto: 400,
+    qualidadeDoProduto: 80,
+    status: true
+    },
+    {
+    nomeDoProduto: 'Mouse',
+    valorDoProduto: 60,
+    qualidadeDoProduto: 70,
+    status: true
+    }
+]
