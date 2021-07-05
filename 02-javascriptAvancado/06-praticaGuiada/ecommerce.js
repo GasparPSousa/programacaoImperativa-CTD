4d80e090f7aa320813754c4f9ead6e867ca05861
GasparPSousa <gasparufrj@gmail.com>
2021-07-05 00:17:38 -0300

    Ecommerce - Item2

--- 02-javascriptAvancado/06-praticaGuiada/ecommerce.js
+++ 02-javascriptAvancado/06-praticaGuiada/ecommerce.js
@@ -21,10 +21,42 @@ produtos = [
     qualidadeDoProduto: 80,
     status: true
     },
+
     {
     nomeDoProduto: 'Mouse',
     valorDoProduto: 60,
     qualidadeDoProduto: 70,
     status: true
-    }
-]
+    },
+
+    {
+    nomeDoProduto: 'Celular',
+    valorDoProduto: 1200,
+    qualidadeDoProduto: 91,
+    status: true
+    },
+];
+console.log('De todos os produtos')
+console.log(produtos)
+
+
+// Item 2
+
+const carrinho = produtos.filter((produto) => {
+    const valor = produto.valorDoProduto;
+    const qualidade = produto.qualidadeDoProduto;
+    const status = produto.status;
+
+    const itensSelecionados = (valor >= 482 && valor <= 1600) && (qualidade >= 60) && (status == true);
+
+    return itensSelecionados;
+
+})
+
+console.log('\nDo carrinho')
+console.log(carrinho)
+
+
+
+
+
