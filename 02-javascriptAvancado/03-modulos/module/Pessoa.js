9c7328873f2bf321cea70a40c187a61e3edf1c4c
GasparPSousa <gasparufrj@gmail.com>
2021-07-10 01:40:17 -0300

    Outra forma de modularizar

---
+++ 02-javascriptAvancado/03-modulos/module/Pessoa.js
@@ -0,0 +1,18 @@
+class Pessoa {
+
+  constructor(nome) {
+      if (nome === undefined || nome.length <= 0) {
+          throw new Error("O nome é obrigatório para uma Pessoa");
+      }
+      this.nome = nome;
+  }
+
+  apresentar() {
+      console.log(`Olá, meu nome é ${this.nome}`);
+  }
+
+}
+
+
+
+export default Pessoa
