579c1ddf8379754ac8143dcbbca7f507ead2af3b
GasparPSousa <gasparufrj@gmail.com>
2021-07-09 23:59:12 -0300

    Novo exemplo de Factory Functions

--- 03-assincronismoLeituraDeArquivo/objetos/factory.js
+++ 03-assincronismoLeituraDeArquivo/objetos/factory.js
@@ -23,3 +23,42 @@ const createCat = name => ({
 
 const myCat2 = createCat('ThunderCat')
 console.log(myCat2.getName())
+
+
+/* Novo exemplo de Factory Functions */
+function criarObjeto() {
+    let objeto  = {}
+    return objeto;
+}
+
+console.log(criarObjeto())
+
+
+pessoaA = criarObjeto()
+pessoaB = criarObjeto()
+
+console.log(pessoaA)
+console.log(pessoaB)
+
+pessoaA.nome = 'Gaspar'
+pessoaB.nome = 'Cristina'
+
+console.log(pessoaA)
+console.log(pessoaB)
+
+function fabricarPessoa(nome, sobrenome) {
+    let pessoa = {};
+    pessoa.nome = nome;
+    pessoa.sobrenome = sobrenome;
+
+    return pessoa;
+}
+
+pessoaA = fabricarPessoa('Gaspar', 'Sousa')
+pessoaB = fabricarPessoa('Cristina', 'Salles')
+
+console.log(pessoaA)
+console.log(pessoaB)
+
+console.log(pessoaA.nome)
+console.log(pessoaB.sobrenome)
