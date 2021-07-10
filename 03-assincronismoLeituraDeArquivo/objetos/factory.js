e87f5233a33ef9dc4c1766a581bf1078b7c7c501
GasparPSousa <gasparufrj@gmail.com>
2021-07-10 00:08:28 -0300

    Adicionando uma nova function dentro da factory function

--- 03-assincronismoLeituraDeArquivo/objetos/factory.js
+++ 03-assincronismoLeituraDeArquivo/objetos/factory.js
@@ -25,6 +25,7 @@ const myCat2 = createCat('ThunderCat')
 console.log(myCat2.getName())
 
 
+
 /* Novo exemplo de Factory Functions */
 function criarObjeto() {
     let objeto  = {}
@@ -51,6 +52,12 @@ function fabricarPessoa(nome, sobrenome) {
     pessoa.nome = nome;
     pessoa.sobrenome = sobrenome;
 
+    function nomeCompleto() {
+        return `${pessoa.nome} ${pessoa.sobrenome}`
+    }
+
+    pessoa.nomeCompleto = nomeCompleto;
+
     return pessoa;
 }
 
@@ -62,3 +69,9 @@ console.log(pessoaB)
 
 console.log(pessoaA.nome)
 console.log(pessoaB.sobrenome)
+
+console.log(pessoaA.nomeCompleto())
+console.log(pessoaB.nomeCompleto())
+
+let pessoaC = fabricarPessoa('Sandra', 'Salles')
+console.log(pessoaC.nomeCompleto())
